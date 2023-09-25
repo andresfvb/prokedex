1e244597e46a9abea685e9216eb0cf216fe1f75a
andresfvb <barandicandres@gmail.com>
2023-09-25 16:07:28 -0500

    Finalizamos MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,20 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    PokemonModule
+
+    MongooseModule.forRoot('mongodb://192.168.0.12:27017/nest-pokemon'),
+
+    PokemonModule,
+
+    CommonModule
   ],
 })
 export class AppModule { }
